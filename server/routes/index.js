5fbff0d65d06b655e575e88c4b832746998d29cc
sesangbit56 <sesangbit56@gmail.com>
2021-05-29 12:02:13 +0000

    login register api added

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const services = require("../services/render");
 const mysql = require("mysql");
+const sha256 = require("sha256");
 const db = mysql.createConnection({
   host : "localhost",
     user : "root",
@@ -17,28 +18,71 @@ router.get('/', services.homeRoutes);
 
 router.get('/register', services.registerRoutes);
 
+/* register api */
 router.post("/register", function(req, res) {
-  console.log('ASDF')
     const id = req.body.id || '';
     const password = req.body.password || '';
     const name = req.body.name || '';
-    const age = parseInt(req.body.age);
+    const age = req.body.age || '';
 
-    console.log(req.body);
-    console.log(password);
-
-    if(!id.length || !password.length || !name.length || !age.length) {
+    if(id.length == 0 || password.length == 0 || name.length == 0 || age.length == 0) {
       return res.status(400).json({err: 'Incorrect info'});
     }
 
-    var sql = `INSERT INTO users(id, name, password, age) VALUES(${id, name, password, age})`;
+    var sql = `INSERT INTO users (id, name, password, age) VALUES("${id}", "${name}", "${sha256(password)}", ${parseInt(age)})`;
     db.query(sql, function(err, rows, fields) {
       if(err) {
-        return res.status(400).json({err: "Sonething went wrong"});
+        console.log(err);
+        return res.status(400).json({'register': false});
       } else {
-        return res.status(201).json({"auto_Increment" : rows.insertId});
+        return res.status(201).json({'register': true});
       }
     })
+});
+
+router.post("/login", function (req, res) {
+  const sql = `SELECT password FROM users WHERE id = "${req.body.id}"`;
+  db.query(sql, (err, rows) => {
+      if (err)
+          return res.status(400).json({
+              result: "sql error"
+          });
+      else {
+          try {
+              if (rows.pw === sha256(req.body.pw)) {
+                  let token = jwt.sign({ name: req.body.id }, "ang");
+                  return res.status(200).json({
+                      login: true,
+                      token: token
+                  })
+              }
+              else {
+                  return res.status(404).json({
+                      result: "invalid id"
+                  })
+              }
+          }
+          catch (e) {
+              console.log(e)
+              return res.status(400).json({
+                  login: false
+              })
+          }
+      }
+  });
+});
+
+router.get("/login", function (req, res) {
+  try {
+      return res.status(200).json({
+          user: jwt.verify(req.headers.authorization.split(' ')[1], "ang")["name"]
+      });
+  }
+  catch(e) {
+      return res.status(401).json({
+          login: false
+      })
+  }
 });
 
 
