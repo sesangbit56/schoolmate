22914ebc8c8e9ebc48d38df62adf464fcf473b6f
sesangbit56 <sesangbit56@gmail.com>
2021-06-07 03:09:05 +0900

    추천 질문 로직 완성

--- server/services/render.js
+++ server/services/render.js
@@ -12,53 +12,74 @@ db.connect();
 exports.homeRoutes = (req, res) => {
   console.log("homeRoutes----------------------------------------------------");
   console.log("   SessionId checking...");
-  if (!req.cookies.sessionId) {
-    console.log("   !!!!SessionId is empty!!!!");
-    res.render("index", {
-      status: false,
-      name: "",
-    });
-  } else {
-    try {
-      var decodedToken = jwt.verify(req.cookies.sessionId, "ang")["uid"];
-    } catch (err) {
-      //when sessionId is not valid
-      console.log(err);
-      res.clearCookie("sessionId");
-      res.render("index", {
-        status: false,
-        name: "",
-      });
-    }
-    console.log(decodedToken);
-    const query = `select * from users where uid = ${decodedToken}`;
-
-    db.query(query, (err, rows, fields) => {
+  var questions = undefined;
+  db.query(
+    `select pid, title, writer_id from questions where pid between 30 and 39`,
+    (err, rows, fields) => {
+      console.log(rows);
       if (err) {
-        console.log(err);
-        res.clearCookie("sessionId");
         res.render("index", {
           status: false,
           name: "",
         });
       } else {
-        if (!rows.length) {
-          console.log("   !!!!SessionId is Invalid!!!!");
-          res.clearCookie("sessionId");
+        questions = JSON.parse(JSON.stringify(rows));
+        console.log(questions);
+        if (!req.cookies.sessionId) {
+          console.log("   !!!!SessionId is empty!!!!");
           res.render("index", {
             status: false,
             name: "",
+            questions: questions,
           });
         } else {
-          console.log("   SessionId Confirmed!");
-          res.render("index", {
-            status: true,
-            name: rows[0].name,
+          try {
+            var decodedToken = jwt.verify(req.cookies.sessionId, "ang")["uid"];
+          } catch (err) {
+            //when sessionId is not valid
+            console.log(err);
+            res.clearCookie("sessionId");
+            res.render("index", {
+              status: false,
+              name: "",
+              questions: questions,
+            });
+          }
+          console.log(decodedToken);
+          const query = `select * from users where uid = ${decodedToken}`;
+
+          db.query(query, (err, rows, fields) => {
+            if (err) {
+              console.log(err);
+              res.clearCookie("sessionId");
+              res.render("index", {
+                status: false,
+                name: "",
+                questions: questions,
+              });
+            } else {
+              if (!rows.length) {
+                console.log("   !!!!SessionId is Invalid!!!!");
+                res.clearCookie("sessionId");
+                res.render("index", {
+                  status: false,
+                  name: "",
+                  questions: questions,
+                });
+              } else {
+                console.log("   SessionId Confirmed!");
+                res.render("index", {
+                  status: true,
+                  name: rows[0].name,
+                  questions: questions,
+                });
+              }
+            }
           });
         }
       }
-    });
-  }
+    }
+  );
 };
 
 exports.signRoutes = (req, res) => {
