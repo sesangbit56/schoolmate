964b4c81154ce29bcb44e3a9fe4cd811f861a83d
sesangbit56 <sesangbit56@gmail.com>
2021-06-06 21:57:34 +0900

    LocalStroage에서 Cookie 방식으로 변경

--- public/javascript/reload.js
+++ public/javascript/reload.js
@@ -1,25 +1,30 @@
-$(document).ready(() => {
-  fetch("/login", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      authorization: localStorage.getItem("sessionId"),
-    },
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      if (response.login) {
-        // localstorage, 리다이렉션
-        console.log(response);
-        fetch("/signIn", {
-          method: "POST",
-          body: JSON.stringify(response),
-          headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            datatype: "text",
-          },
-        });
-        console.log("sucess");
-      }
-    });
-});
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// $(document).ready(() => {
+//   fetch("/login", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json; charset=utf-8",
+//       authorization: localStorage.getItem("sessionId"),
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((response) => {
+//       if (response.login) {
+//         // localstorage, 리다이렉션
+//         console.log(response);
+//         fetch("/signIn", {
+//           method: "POST",
+//           body: JSON.stringify(response),
+//           headers: {
+//             "Content-Type": "application/json; charset=utf-8",
+//             datatype: "text",
+//           },
+//         });
+
+//         console.log("sucess");
+//       }
+//     });
+// });
--- server/services/render.js
+++ server/services/render.js
@@ -1,15 +1,52 @@
+const jwt = require("jsonwebtoken");
+const querystring = require("querystring");
+const mysql = require("mysql");
+const db = mysql.createConnection({
+  host: "158.247.222.53",
+  user: "dev",
+  password: "13975",
+  database: "schoolmate",
+});
+db.connect();
+
 exports.homeRoutes = (req, res) => {
-  console.log(req.session);
-  const status = req.session.login;
-  const name = req.session.name;
-  console.log(name);
-  console.log(status);
-  req.session.login = undefined;
-  req.session.name = undefined;
-  res.render("index", {
-    status: status,
-    name: name,
-  });
+  console.log("homeRoutes----------------------------------------------------");
+  console.log("   SessionId checking...");
+  if (!req.cookies.sessionId) {
+    console.log("   !!!!SessionId is empty!!!!");
+    res.render("index");
+  } else {
+    try {
+      var decodedToken = jwt.verify(req.cookies.sessionId, "ang")["uid"];
+    } catch (err) {
+      //when sessionId is not valid
+      console.log(err);
+      res.clearCookie("sessionId");
+      res.render("index");
+    }
+    console.log(decodedToken);
+    const query = `select * from users where uid = ${decodedToken}`;
+
+    db.query(query, (err, rows, fields) => {
+      if (err) {
+        console.log(err);
+        res.clearCookie("sessionId");
+        res.render("index");
+      } else {
+        if (!rows.length) {
+          console.log("   !!!!SessionId is Invalid!!!!");
+          res.clearCookie("sessionId");
+          res.render("index");
+        } else {
+          console.log("   SessionId Confirmed!");
+          res.render("index", {
+            status: true,
+            name: rows[0].name,
+          });
+        }
+      }
+    });
+  }
 };
 
 exports.signRoutes = (req, res) => {
