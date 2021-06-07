a364fe6fa863473e37fee0e27b197caf229113fc
jungin7612 <jungin7612@gmail.com>
2021-06-07 22:46:15 +0900

    tmp commit

--- server/services/render.js
+++ server/services/render.js
@@ -85,3 +85,76 @@ exports.homeRoutes = (req, res) => {
 exports.signRoutes = (req, res) => {
   res.render("sign");
 };
+
+exports.qnaRoutes = (req, res) => {
+  console.log("qnaRoutes----------------------------------------------------");
+  console.log("   SessionId checking...");
+  var questions = undefined;
+  db.query(
+    `select pid, title, writer_id from questions where pid between 30 and 39`,
+    (err, rows, fields) => {
+      console.log(rows);
+      if (err) {
+        res.render("qna", {
+          status: false,
+          name: "",
+        });
+      } else {
+        questions = JSON.parse(JSON.stringify(rows));
+        console.log(questions);
+        if (!req.cookies.sessionId) {
+          console.log("   !!!!SessionId is empty!!!!");
+          res.render("qna", {
+            status: false,
+            name: "",
+            questions: questions,
+          });
+        } else {
+          try {
+            var decodedToken = jwt.verify(req.cookies.sessionId, "ang")["uid"];
+          } catch (err) {
+            //when sessionId is not valid
+            console.log(err);
+            res.clearCookie("sessionId");
+            res.render("qna", {
+              status: false,
+              name: "",
+              questions: questions,
+            });
+          }
+          console.log(decodedToken);
+          const query = `select * from users where uid = ${decodedToken}`;
+
+          db.query(query, (err, rows, fields) => {
+            if (err) {
+              console.log(err);
+              res.clearCookie("sessionId");
+              res.render("qna", {
+                status: false,
+                name: "",
+                questions: questions,
+              });
+            } else {
+              if (!rows.length) {
+                console.log("   !!!!SessionId is Invalid!!!!");
+                res.clearCookie("sessionId");
+                res.render("qna", {
+                  status: false,
+                  name: "",
+                  questions: questions,
+                });
+              } else {
+                console.log("   SessionId Confirmed!");
+                res.render("qna", {
+                  status: true,
+                  name: rows[0].name,
+                  questions: questions,
+                });
+              }
+            }
+          });
+        }
+      }
+    }
+  );
+};
