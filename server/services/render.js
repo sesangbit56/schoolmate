34c4c83dcea9c88f03354c8b197c367268d037b9
sesangbit56 <sesangbit56@gmail.com>
2021-06-08 03:26:44 +0900

    render.js의 간결화 및 비동기화 완료

--- server/services/render.js
+++ server/services/render.js
@@ -1,147 +1,40 @@
-const jwt = require("jsonwebtoken");
 const querystring = require("querystring");
-const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: "158.247.222.53",
-  user: "dev",
-  password: "13975",
-  database: "schoolmate",
-});
-db.connect();
+const url = require("url");
 
 const tokenCheck = require("./tokenCheck");
+const questionCheck = require("./questionCheck");
 
-exports.homeRoutes = (req, res) => {
+exports.homeRoutes = async (req, res) => {
   console.log("homeRoutes----------------------------------------------------");
   console.log("   SessionId checking...");
-  var questions = undefined;
-  db.query(
-    `select pid, title, writer_id from questions where pid between 30 and 39`,
-    (err, rows, fields) => {
-      if (err) {
-        res.render("index", {
-          status: false,
-          name: "",
-          questions: questions,
-        });
-      } else {
-        questions = JSON.parse(JSON.stringify(rows));
-        if (!req.cookies.sessionId) {
-          console.log("   !!!!SessionId is empty!!!!");
-          res.render("index", {
-            status: false,
-            name: "",
-            questions: questions,
-          });
-        } else {
-          try {
-            var decodedToken = jwt.verify(req.cookies.sessionId, "ang")["uid"];
-          } catch (err) {
-            //when sessionId is not valid
-            console.log(err);
-            res.clearCookie("sessionId");
-            res.render("index", {
-              status: false,
-              name: "",
-              questions: questions,
-            });
-          }
-          console.log(decodedToken);
-          const query = `select * from users where uid = ${decodedToken}`;
 
-          db.query(query, (err, rows, fields) => {
-            if (err) {
-              console.log(err);
-              res.clearCookie("sessionId");
-              res.render("index", {
-                status: false,
-                name: "",
-                questions: questions,
-              });
-            } else {
-              if (!rows.length) {
-                console.log("   !!!!SessionId is Invalid!!!!");
-                res.clearCookie("sessionId");
-                res.render("index", {
-                  status: false,
-                  name: "",
-                  questions: questions,
-                });
-              } else {
-                console.log("   SessionId Confirmed!");
-                res.render("index", {
-                  status: true,
-                  name: rows[0].name,
-                  questions: questions,
-                });
-              }
-            }
-          });
-        }
-      }
-    }
-  );
+  const status = await tokenCheck.setStatus(req.cookies.sessionId);
+  const name = await tokenCheck.setName(req.cookies.sessionId);
+  const questions = await questionCheck.getRecommendedQuestions();
+
+  res.render("index", {
+    status: status,
+    name: name,
+    questions: questions,
+  });
 };
 
 exports.signRoutes = (req, res) => {
   res.render("sign");
 };
 
-exports.qnaRoutes = (req, res) => {
+exports.qnaRoutes = async (req, res) => {
   console.log("qnaRoutes----------------------------------------------------");
-  console.log("   SessionId checking...");
+  var parsedUrl = url.parse(req.url);
+  const page = querystring.parse(parsedUrl.query, "&", "=").list;
 
-  tokenCheck.checkSessionId(req.cookies.sessionId, function (decodedToken) {});
-  // if (!req.cookies.sessionId) {
-  //   console.log("   !!!!SessionId is empty!!!!");
-  //   res.render("qna", {
-  //     status: false,
-  //     name: "",
-  //     qnaList: "",
-  //   });
-  // } else {
-  //   try {
-  //     var decodedToken = jwt.verify(req.cookies.sessionId, "ang")["uid"];
-  //   } catch (err) {
-  //     //when sessionId is not valid
-  //     console.log(err);
-  //     res.clearCookie("sessionId");
-  //     res.render("qna", {
-  //       status: false,
-  //       name: "",
-  //       qnaList: "",
-  //     });
-  //   }
-  //   console.log(decodedToken);
-  //   const query = `select * from users where uid = ${decodedToken}`;
+  const status = await tokenCheck.setStatus(req.cookies.sessionId);
+  const name = await tokenCheck.setName(req.cookies.sessionId);
+  const qnaList = await questionCheck.getQnaList(page);
 
-  //   db.query(query, (err, rows, fields) => {
-  //     if (err) {
-  //       console.log(err);
-  //       res.clearCookie("sessionId");
-  //       res.render("qna", {
-  //         status: false,
-  //         name: "",
-  //         qnaList: "",
-  //       });
-  //     } else {
-  //       if (!rows.length) {
-  //         console.log("   !!!!SessionId is Invalid!!!!");
-  //         res.clearCookie("sessionId");
-  //         res.render("qna", {
-  //           status: false,
-  //           name: "",
-  //           qnaList: "",
-  //         });
-  //       } else {
-  //         console.log("   SessionId Confirmed!");
-  //         res.render("qna", {
-  //           status: true,
-  //           name: rows[0].name,
-  //           qnaList: "",
-  //         });
-  //       }
-  //     }
-  //   });
-  // }
+  res.render("qna", {
+    status: status,
+    name: name,
+    qnaList: qnaList,
+  });
 };
--- server/services/tokenCheck.js
+++ server/services/tokenCheck.js
@@ -1,16 +1,57 @@
 const jwt = require("jsonwebtoken");
+const mysql = require("mysql");
+const db = mysql.createConnection({
+  host: "158.247.222.53",
+  user: "dev",
+  password: "13975",
+  database: "schoolmate",
+});
+db.connect();
 
-exports.checkSessionId = function (sessionId, callbackFunc) {
-  if (!sessionID) {
-    console.log("   !!!!SessionId is empty!!!!");
-    callbackFunc(0);
-  } else {
-    try {
-      var decodedToken = jwt.verify(sessionId, "ang")["uid"];
-      callbackFunc(decodedToken);
-    } catch (e) {
-      console.log(e);
-      callbackFunc(0);
+exports.setStatus = function (sessionId) {
+  return new Promise((resolve) => {
+    if (!sessionId) {
+      resolve(false);
+    } else {
+      try {
+        var decodedToken = jwt.verify(sessionId, "ang")["uid"];
+        db.query(
+          `select name from users where uid = ${decodedToken}`,
+          (err, rows) => {
+            if (err || !rows.length) {
+              resolve(false);
+            } else {
+              resolve(true);
+            }
+          }
+        );
+      } catch (e) {
+        resolve(false);
+      }
     }
-  }
+  });
+};
+
+exports.setName = function (sessionId) {
+  return new Promise((resolve) => {
+    if (!sessionId) {
+      resolve("");
+    } else {
+      try {
+        var decodedToken = jwt.verify(sessionId, "ang")["uid"];
+        db.query(
+          `select name from users where uid = ${decodedToken}`,
+          (err, rows) => {
+            if (err || !rows.length) {
+              resolve("");
+            } else {
+              resolve(rows[0].name);
+            }
+          }
+        );
+      } catch (e) {
+        resolve("");
+      }
+    }
+  });
 };
