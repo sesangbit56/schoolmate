3e6e7df3d6829df7dffbc2a05b366e6254b1e089
sesangbit56 <sesangbit56@gmail.com>
2021-06-09 02:22:21 +0900

    검색 기능 추가

--- server/services/questionCheck.js
+++ server/services/questionCheck.js
@@ -7,48 +7,110 @@ const db = mysql.createConnection({
 });
 db.connect();
 
-const getQnaCount = () => {
+//complete
+const buildSearchQuery = (search) => {
   return new Promise((resolve) => {
-    db.query(`select count(*) as cnt from questions`, (err, rows) => {
-      if (err) {
-        console.log(err);
-        resolve(0);
-      } else {
-        resolve(rows[0].cnt);
-      }
+    let searchQuery = ``;
+    if (!search) {
+      resolve(searchQuery);
+    }
+
+    const words = search.split(" ");
+    searchQuery = `where title regexp '${words[0]}`;
+    for (let i = 1; i < words.length; i++) {
+      searchQuery += `|${words[i]}`;
+    }
+    searchQuery += `'`;
+    resolve(searchQuery);
+  });
+};
+
+//complete
+const getQnaCount = (search) => {
+  return new Promise((resolve) => {
+    buildSearchQuery(search).then((searchQuery) => {
+      db.query(
+        `select count(*) as cnt from questions ` + searchQuery,
+        (err, rows) => {
+          if (err) {
+            console.log(err);
+            resolve(0);
+          } else {
+            console.log(rows[0].cnt);
+            resolve(rows[0].cnt);
+          }
+        }
+      );
     });
   });
 };
 
-exports.getQnaList = (list) => {
+//complete
+exports.getQnaPageList = (list, search) => {
   return new Promise((resolve) => {
-    getQnaCount().then((count) => {
-      if (Math.ceil(count / 25) >= list && list > 0) {
-        let qnaList = ``;
-        db.query(
-          `select category, pid, title from questions order by pid desc limit ${
-            (list - 1) * 25
-          }, 25 `,
-          (err, rows) => {
-            if (err) {
-              resolve(qnaList);
-            } else {
-              for (let i = 0; i < rows.length; i++) {
-                qnaList =
-                  qnaList +
-                  `<a class="question-container" href="/qna/detail/${rows[i].pid}">
-                        <p class="category">[${rows[i].category}]</p>
-                        <p class="qnaTitle">Q: ${rows[i].title}</p>
-                      </a>`;
-              }
-              resolve(qnaList);
-            }
-          }
-        );
-      } else {
-        console.log("what the fuck");
-        resolve(qnaList);
+    getQnaCount(search).then((count) => {
+      let qnaPageList = ``;
+      if (!search) {
+        for (let i = list - 6 < 1 ? 1 : list - 5; i < list; i++) {
+          qnaPageList += `<div><a href="/qna?list=${i}">${i}</a></div>`;
+        }
+        for (
+          let i = list;
+          i <= Math.ceil(count / 25) && i < parseInt(list) + 5;
+          i++
+        ) {
+          qnaPageList += `<div><a href="/qna?list=${i}">${i}</a></div>`;
+        }
+        console.log(qnaPageList);
+        resolve(qnaPageList);
       }
+      for (let i = list - 6 < 1 ? 1 : list - 5; i < list; i++) {
+        qnaPageList += `<div><a href="/qna?search=${search}&list=${i}">${i}</a></div>`;
+      }
+      for (
+        let i = list;
+        i <= Math.ceil(count / 25) && i < parseInt(list) + 5;
+        i++
+      ) {
+        qnaPageList += `<div><a href="/qna?search=${search}&list=${i}">${i}</a></div>`;
+      }
+      console.log(qnaPageList);
+      resolve(qnaPageList);
+    });
+  });
+};
+
+exports.getQnaList = (list, search) => {
+  return new Promise((resolve) => {
+    getQnaCount(search).then((count) => {
+      let qnaList = ``;
+      buildSearchQuery(search).then((searchQuery) => {
+        if (Math.ceil(count / 25) >= list && list > 0) {
+          db.query(
+            `select category, pid, title from questions ` +
+              searchQuery +
+              ` order by pid desc limit ${(list - 1) * 25}, 25 `,
+            (err, rows) => {
+              if (err) {
+                resolve(qnaList);
+              } else {
+                for (let i = 0; i < rows.length; i++) {
+                  qnaList =
+                    qnaList +
+                    `<a class="question-container" href="/qna/detail/${rows[i].pid}">
+                          <p class="category">[${rows[i].category}]</p>
+                          <p class="qnaTitle">Q: ${rows[i].title}</p>
+                        </a>`;
+                }
+                resolve(qnaList);
+              }
+            }
+          );
+        } else {
+          console.log("what the fuck");
+          resolve(qnaList);
+        }
+      });
     });
   });
 };
