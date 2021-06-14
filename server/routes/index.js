a262e7db1333e41cf195529e1a7937391c5c6807
sesangbit56 <sesangbit56@gmail.com>
2021-06-14 22:30:53 +0900

    add : added answer post

--- server/routes/index.js
+++ server/routes/index.js
@@ -33,6 +33,8 @@ router.get("/qna/list", controller.qnaListGetControll);
 
 router.post("/qna/detail/api", controller.questionGetControll);
 
+router.post("/qna/detail/answer", controller.answerPostControll);
+
 router.delete("/qna/detail/:pid", controller.questionDeleteControll);
 
 router.put("/qna/detail/:pid", controller.questionPutControll);
